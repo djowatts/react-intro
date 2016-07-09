45a9cd447a45ac9e4c36db8e99458fa306515352
David Watts <djowatts@googlemail.com>
2016-07-09 17:59:22 +0100

    Allowing the comments to be loaded from the server

--- src/public/scripts/comment-box.js
+++ src/public/scripts/comment-box.js
@@ -49,17 +49,37 @@ var CommentForm = React.createClass({
 });
 
 var CommentBox = React.createClass({
+    loadFromServer: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentDidMount: function() {
+        this.loadFromServer();
+        setInterval(this.loadFromServer, this.props.pollInterval);
+    },
     render: function(){
         return(
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList data={this.props.data} />
+                <CommentList data={this.state.data} />
                 <CommentForm />
             </div>
         );
     }
 });
 ReactDOM.render(
-    <CommentBox data={data}/>,
+    <CommentBox url="/api/comments" pollInterval="2000" />,
     document.getElementById('content')
 );
